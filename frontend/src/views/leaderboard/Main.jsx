7122f656b7b4df32b4f3f644a2cdf5a955d360ad
imdavyking <yungdynamic53@gmail.com>
2025-01-30 22:38:36 +0100

    updated terms

--- frontend/src/views/leaderboard/Main.jsx
+++ frontend/src/views/leaderboard/Main.jsx
@@ -26,7 +26,7 @@ const PointsList = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-4xl mx-auto p-4 ">
       <h2 className="text-3xl font-bold text-center text-gray-800 mb-4">
         Leaderboard
       </h2>
@@ -75,23 +75,6 @@ const PointsList = () => {
       </div>
     </div>
   );
-  //   return (
-  //     <div>
-  //       <h2>Points List</h2>
-  //       <ul>
-  //         {data.pointsAddeds.nodes.map((item) => (
-  //           <li key={item.id}>
-  //             <strong>User:</strong> {item.user} <br />
-  //             <strong>Points:</strong> {item.points} <br />
-  //             <strong>Contract:</strong> {item.contractAddress} <br />
-  //             <strong>Updated:</strong>{" "}
-  //             {new Date(item.updatedTimeStamp * 1000).toLocaleString()}
-  //             <hr />
-  //           </li>
-  //         ))}
-  //       </ul>
-  //     </div>
-  //   );
 };
 
 export default PointsList;
