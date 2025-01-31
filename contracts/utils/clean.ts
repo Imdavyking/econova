b76da77c5ba6dac7ad289f78e578d79241c14724
imdavyking <yungdynamic53@gmail.com>
2025-01-31 10:24:32 +0100

    fix: used try block for cleaning

--- contracts/utils/clean.ts
+++ contracts/utils/clean.ts
@@ -2,14 +2,18 @@ import path from "path"
 import fs from "fs"
 
 export const cleanDeployments = (chainId: number | string) => {
-    const ignitionDeployments = path.join(
-        process.cwd(),
-        "ignition",
-        "deployments",
-        `chain-${chainId}`
-    )
-    if (fs.existsSync(ignitionDeployments)) {
-        console.log(`🗑️ Deleting folder: ${ignitionDeployments}`)
-        fs.rmSync(ignitionDeployments, { recursive: true, force: true })
+    try {
+        const ignitionDeployments = path.join(
+            process.cwd(),
+            "ignition",
+            "deployments",
+            `chain-${chainId}`
+        )
+        if (fs.existsSync(ignitionDeployments)) {
+            console.log(`🗑️ Deleting folder: ${ignitionDeployments}`)
+            fs.rmSync(ignitionDeployments, { recursive: true, force: true })
+        }
+    } catch (error) {
+        console.error("Error cleaning the database", error)
     }
 }
--- indexer/src/utils/clean.ts
+++ indexer/src/utils/clean.ts
@@ -2,9 +2,13 @@ import path from "path";
 import fs from "fs";
 
 export const cleanDB = () => {
-  const ignitionDeployments = path.join(process.cwd(), ".data");
-  if (fs.existsSync(ignitionDeployments)) {
-    console.log(`🗑️ Deleting folder: ${ignitionDeployments}`);
-    fs.rmSync(ignitionDeployments, { recursive: true, force: true });
+  try {
+    const ignitionDeployments = path.join(process.cwd(), ".data");
+    if (fs.existsSync(ignitionDeployments)) {
+      console.log(`🗑️ Deleting folder: ${ignitionDeployments}`);
+      fs.rmSync(ignitionDeployments, { recursive: true, force: true });
+    }
+  } catch (error) {
+    console.error("Error cleaning the database", error);
   }
 };
