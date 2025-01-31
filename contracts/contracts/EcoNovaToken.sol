68acda3c2b3395980c2fe14842c554a233f2ff3d
imdavyking <yungdynamic53@gmail.com>
2025-01-31 11:21:56 +0100

    chore: updated code

--- contracts/contracts/EcoNovaToken.sol
+++ contracts/contracts/EcoNovaToken.sol
@@ -4,7 +4,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract EcoNovaToken is ERC20 {
     address public immutable OWNER;
+    uint256 public constant MAX_SUPPLY = 21_000_000 * 10 ** 18; // 1 million tokens with 18 decimals
+
     error EcoNovaToken__NotOwner();
+    error EcoNovaToken__MaxSupplyExceeded();
 
     constructor() ERC20("EcoNovaToken", "ENT") {
         OWNER = msg.sender;
@@ -13,6 +16,9 @@ contract EcoNovaToken is ERC20 {
     function mint(address to, uint256 amount) external {
         if (msg.sender != OWNER) {
             revert EcoNovaToken__NotOwner();
+        }
+        if (totalSupply() + amount > MAX_SUPPLY) {
+            revert EcoNovaToken__MaxSupplyExceeded();
         }
         _mint(to, amount);
     }
