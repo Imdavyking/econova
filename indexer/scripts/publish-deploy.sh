21fe61ba4e1ea44a6b279b9c31a1c2f35d5ab9e5
imdavyking <yungdynamic53@gmail.com>
2025-02-01 23:32:34 +0100

    chore: updated bash file

--- indexer/scripts/publish-deploy.sh
+++ indexer/scripts/publish-deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-while getopts p:o: flag
+while getopts p:o:e flag
 do
     case "${flag}" in
         p) PROJECTNAME=${OPTARG};;
         o) ORG=${OPTARG};;
-        *) echo "Usage: $0 [-p projectname] [-o org]" && exit 1;;
+        e) ENDPOINT=${OPTARG};;
+        *) echo "Usage: $0 [-p projectname] [-o org] [-e endpoint]" && exit 1;;
     esac
 done
 
 IPFSCID=$(npx subql publish -o -f .)
 
-npx subql deployment:deploy -d --ipfsCID="$IPFSCID" --projectName="${PROJECTNAME}" --org="${ORG%/*}"
+npx subql deployment:deploy -d --ipfsCID="$IPFSCID" --projectName="${PROJECTNAME}" --org="${ORG%/*}" --endpoint="${ENDPOINT}"
