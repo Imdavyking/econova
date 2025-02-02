da2fe856209d4c04000573788c02fb23da631824
imdavyking <yungdynamic53@gmail.com>
2025-02-02 18:10:43 +0100

    chore: updated details

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -1,14 +1,32 @@
 // getUserTwitterInfo
 
 import { Request, Response } from "express";
-
+import { LoginWithTwitter } from "../services/login.twitter.services";
+export const twitterLogin = new LoginWithTwitter({
+  callbackUrl: "http://localhost:3100/twitter/callback",
+});
 export const getUserTwitterInfo = async (req: Request, res: Response) => {
-  // if (!req.session.user) {
-  //   res.status(401).json({
-  //     error: "Unauthorized",
-  //   });
-  //   return;
-  // }
+  try {
+    const user = req.cookies.user;
 
-  res.json(req.cookies);
+    if (!user) {
+      res.status(401).json({
+        error: "Unauthorized",
+      });
+      return;
+    }
+
+    const userInfo = JSON.parse(user);
+
+    const userTwitterId = await twitterLogin.validateUserToken(
+      userInfo.userToken,
+      userInfo.userTokenSecret
+    );
+
+    console.log({ userTwitterId });
+
+    res.json({ userTwitterId });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
 };
