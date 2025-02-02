c0bd657ed54132f9ed870550cdbad4ee9392062c
imdavyking <yungdynamic53@gmail.com>
2025-02-02 21:22:57 +0100

    chore: updated settings

--- frontend/src/views/earn-points/Main.jsx
+++ frontend/src/views/earn-points/Main.jsx
@@ -1,42 +1,12 @@
-import React, { useEffect } from "react";
-import { gql, useQuery } from "@apollo/client";
-import { APP_NAME, CONTRACT_ADDRESS } from "../../utils/constants";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { APP_NAME } from "../../utils/constants";
 import logoUrl from "@/assets/images/logo.png";
 import DarkModeSwitcher from "@/components/dark-mode-switcher/Main";
-import { ellipsify } from "../../utils";
-import { FaSpinner } from "react-icons/fa";
 import { getAllTweets } from "../../services/tweets.services";
 import { Tweet } from "../../components/tweet";
-// GraphQL Query
-const GET_POINTS = gql`
-  query MyQuery {
-    pointsAddeds(orderBy: POINTS_DESC, first: 10) {
-      nodes {
-        id
-        contractAddress
-        points
-        updatedTimeStamp
-        user
-      }
-    }
-  }
-`;
-const tweets = [
-  {
-    _id: "67950c24e1289710853e35af",
-    edit_history_tweet_ids: ["1883184787340349875"],
-    id: "1883184787340349875",
-    text: "In a digital realm filled with complexities, EcoNova serves as the savvy navigator guiding you through the maze of blockchain wonders with wit and wisdom. Step into the world of smart contracts and innovation, where every byte holds a story waiting to be unraveled.",
-  },
-  {
-    _id: "12345c24e1289710853e35af",
-    edit_history_tweet_ids: ["1883184787340349876"],
-    id: "1883184787340349876",
-    text: "Excited to explore new opportunities in blockchain! #innovation #smartcontracts",
-  },
-];
 
-const TweetList = () => {
+const TweetList = ({ tweets }) => {
   return (
     <div className="container mx-auto px-4">
       <div className="flex flex-col gap-4">
@@ -49,14 +19,14 @@ const TweetList = () => {
 };
 
 const EarnPoints = () => {
+  const [tweets, setTweets] = useState([]);
+
   useEffect(() => {
     getAllTweets()
-      .then((data) => console.log(data))
+      .then((data) => setTweets(data))
       .catch((error) => console.log(error));
   }, []);
-  const { loading, error, data } = useQuery(GET_POINTS);
 
-  if (error) return <p>Error: {error.message}</p>;
   return (
     <div className="max-w-4xl mx-auto p-4 ">
       <DarkModeSwitcher />
@@ -67,7 +37,7 @@ const EarnPoints = () => {
         </a>
       </h2>
 
-      <TweetList />
+      <TweetList tweets={tweets} />
     </div>
   );
 };
