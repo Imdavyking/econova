efcb59af5ef34710505aaf939a15ecb8f8d779f1
imdavyking <yungdynamic53@gmail.com>
2025-02-02 14:02:25 +0100

    chore: updated

--- frontend/src/views/leaderboard/Main.jsx
+++ frontend/src/views/leaderboard/Main.jsx
@@ -4,6 +4,7 @@ import { APP_NAME, CONTRACT_ADDRESS } from "../../utils/constants";
 import logoUrl from "@/assets/images/logo.png";
 import DarkModeSwitcher from "@/components/dark-mode-switcher/Main";
 import { ellipsify } from "../../utils";
+import { FaSpinner } from "react-icons/fa";
 // GraphQL Query
 const GET_POINTS = gql`
   query MyQuery {
@@ -22,7 +23,7 @@ const GET_POINTS = gql`
 const PointsList = () => {
   const { loading, error, data } = useQuery(GET_POINTS);
 
-  if (loading) return <p>Loading...</p>;
+  // if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
   return (
     <div className="max-w-4xl mx-auto p-4 ">
@@ -34,51 +35,57 @@ const PointsList = () => {
         </a>
       </h2>
 
-      <div className="overflow-x-auto">
-        <table className="w-full border-collapse border border-gray-200 rounded-lg shadow-md">
-          <thead>
-            <tr className="bg-gray-800 text-white">
-              <th className="p-3 text-left">Rank</th>
-              <th className="p-3 text-left">User</th>
-              <th className="p-3 text-left">Points</th>
-              <th className="p-3 text-left">Updated</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.pointsAddeds.nodes
-              .filter((item) => {
-                console.log({ item });
-                return (
-                  String(item.contractAddress).toLowerCase() ==
-                  String(CONTRACT_ADDRESS).toLowerCase()
-                );
-              })
-              .map((item, index) => (
-                <tr
-                  key={item.id}
-                  className={`border-b ${
-                    index === 0
-                      ? "bg-yellow-200"
-                      : index === 1
-                      ? "bg-gray-200"
-                      : index === 2
-                      ? "bg-orange-200"
-                      : "bg-white"
-                  } hover:bg-gray-100 transition`}
-                >
-                  <td className="p-3 text-black">{index + 1}</td>
-                  <td className="p-3 font-semibold text-black">
-                    {ellipsify(item.user)}
-                  </td>
-                  <td className="p-3 font-bold text-blue-600">{item.points}</td>
-                  <td className="p-3 text-black">
-                    {new Date(item.updatedTimeStamp * 1000).toLocaleString()}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
+      {loading ? (
+        <FaSpinner className="w-5 h-5 animate-spin" />
+      ) : (
+        <div className="overflow-x-auto">
+          <table className="w-full border-collapse border border-gray-200 rounded-lg shadow-md">
+            <thead>
+              <tr className="bg-gray-800 text-white">
+                <th className="p-3 text-left">Rank</th>
+                <th className="p-3 text-left">User</th>
+                <th className="p-3 text-left">Points</th>
+                <th className="p-3 text-left">Updated</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.pointsAddeds.nodes
+                .filter((item) => {
+                  console.log({ item });
+                  return (
+                    String(item.contractAddress).toLowerCase() ==
+                    String(CONTRACT_ADDRESS).toLowerCase()
+                  );
+                })
+                .map((item, index) => (
+                  <tr
+                    key={item.id}
+                    className={`border-b ${
+                      index === 0
+                        ? "bg-yellow-200"
+                        : index === 1
+                        ? "bg-gray-200"
+                        : index === 2
+                        ? "bg-orange-200"
+                        : "bg-white"
+                    } hover:bg-gray-100 transition`}
+                  >
+                    <td className="p-3 text-black">{index + 1}</td>
+                    <td className="p-3 font-semibold text-black">
+                      {ellipsify(item.user)}
+                    </td>
+                    <td className="p-3 font-bold text-blue-600">
+                      {item.points}
+                    </td>
+                    <td className="p-3 text-black">
+                      {new Date(item.updatedTimeStamp * 1000).toLocaleString()}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
