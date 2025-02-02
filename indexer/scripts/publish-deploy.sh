c4caf858b64850df66947a8ecff3e114fdf1dc3e
imdavyking <yungdynamic53@gmail.com>
2025-02-02 15:42:53 +0100

    chore: added publish

--- indexer/scripts/publish-deploy.sh
+++ indexer/scripts/publish-deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-while getopts p:o flag
+while getopts p:o:e flag
 do
     case "${flag}" in
         p) PROJECTNAME=${OPTARG};;
         o) ORG=${OPTARG};;
+        e) ENDPOINT=${OPTARG};;
         *) echo "Usage: $0 [-p projectname] [-o org] [-e endpoint]" && exit 1;;
     esac
 done
@@ -13,4 +14,4 @@ IPFSCID=$(npx subql publish -o -f .)
 
 echo $IPFSCID
 
-# npx subql deployment:deploy -d --ipfsCID="$IPFSCID" --projectName="${PROJECTNAME}" --org="${ORG%/*}" --endpoint="https://indexing.onfinality.io"
+npx subql deployment:deploy -d --ipfsCID="$IPFSCID" --projectName="${PROJECTNAME}" --org="${ORG%/*}" --endpoint="${ENDPOINT}"
