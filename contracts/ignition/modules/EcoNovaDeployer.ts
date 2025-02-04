3723fd63d92b75866203109d1f642df382689ccb
imdavyking <yungdynamic53@gmail.com>
2025-02-04 01:47:00 +0100

    chore: updating scripts

--- contracts/ignition/modules/EcoNovaDeployer.ts
+++ contracts/ignition/modules/EcoNovaDeployer.ts
@@ -1,21 +1,20 @@
-// This setup uses Hardhat Ignition to manage smart contract deployments.
-// Learn more about it at https://hardhat.org/ignition
-
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules"
 import dotenv from "dotenv"
 import { ethers, network } from "hardhat"
 import { NamedArtifactContractDeploymentFuture } from "@nomicfoundation/ignition-core"
+
 dotenv.config()
+
 const ecoNovaModule = buildModule("EcoNovaModule", (m) => {
     const chainId = network.config.chainId
-    let oracle: NamedArtifactContractDeploymentFuture<"MockOracleAggregator"> | string =
+    let oracle: NamedArtifactContractDeploymentFuture<"MockPythPriceFeed"> | string =
         process.env.ORACLE_ADDRESS!
 
     const botPrivateKey = process.env.PRIVATE_KEY!
-
     const wallet = new ethers.Wallet(botPrivateKey)
+
     if (chainId === 31337) {
-        oracle = m.contract("MockOracleAggregator", [])
+        oracle = m.contract("MockPythPriceFeed", [])
     }
 
     const ecoNovaDeployer = m.contract("EcoNovaManager", [oracle, wallet.address])
