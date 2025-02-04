a3e3fa05729ca083e57fa1f20bba585667156fbc
imdavyking <yungdynamic53@gmail.com>
2025-02-04 18:13:13 +0100

    chore: updated

--- frontend/src/views/donate/Categories.jsx
+++ frontend/src/views/donate/Categories.jsx
@@ -1,9 +1,10 @@
 import { charityCategories } from "../../utils/charity.categories";
 import CharityCategory from "./Category";
-
+import DarkModeSwitcher from "@/components/dark-mode-switcher/Main";
 export default function CharityCategories() {
   return (
     <div className="max-w-2xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
+      <DarkModeSwitcher />
       <h2 className="text-2xl font-bold text-gray-800 mb-4">
         Charity Categories
       </h2>
