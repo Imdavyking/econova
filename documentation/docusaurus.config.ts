6a15be21c0e4eef81057829a0046eb3bdb88dc8f
imdavyking <yungdynamic53@gmail.com>
2025-02-06 08:23:05 +0100

    chore: updated katex path

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -33,19 +33,24 @@ const config: Config = {
     defaultLocale: "en",
     locales: ["en"],
   },
-
+  stylesheets: [
+    {
+      href: "./src/katex/katex.min.css",
+      type: "text/css",
+    },
+  ],
   presets: [
     [
-      "classic",
+      "@docusaurus/preset-classic",
       {
         docs: {
-          // remarkPlugins: [remarkMath],
-          // rehypePlugins: [rehypeKatex],
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: "./src/css/custom.css",
