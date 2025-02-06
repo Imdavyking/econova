7108465e78d0d98416203a8644487c35cbee466c
imdavyking <yungdynamic53@gmail.com>
2025-02-06 10:34:42 +0100

    chore: verifier working

--- frontend/src/services/zk.bmi.services.js
+++ frontend/src/services/zk.bmi.services.js
@@ -7,4 +7,14 @@ export async function checkBMI() {
 
   console.log("Proof: ");
   console.log(JSON.stringify(proof, null, 1));
+
+  //   const vKey = JSON.parse(fs.readFileSync("verification_key.json"));
+
+  //   const res = await snarkjs.groth16.verify(vKey, publicSignals, proof);
+
+  //   if (res === true) {
+  //     console.log("Verification OK");
+  //   } else {
+  //     console.log("Invalid proof");
+  //   }
 }
