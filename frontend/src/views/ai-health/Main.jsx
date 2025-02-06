ac9c60ec0e32bf8f0fbc5f52af6c6918257f5676
imdavyking <yungdynamic53@gmail.com>
2025-02-07 00:22:02 +0100

    chore: updated ui

--- frontend/src/views/ai-health/Main.jsx
+++ frontend/src/views/ai-health/Main.jsx
@@ -18,19 +18,16 @@ const LeaderBoard = () => {
   const [height, setHeight] = useState("");
   const [suggestion, setSuggestion] = useState("");
   return (
-    <div className="max-w-4xl mx-auto p-4 ">
+    <div className="min-h-screen flex flex-col items-center  p-4">
       <DarkModeSwitcher />
       <h2 className="text-3xl font-bold text-white mb-4 flex flex-col items-center">
         <a href="/" className="flex items-center space-x-3">
           <img alt={APP_NAME} className="w-10" src={logoUrl} />
-          <span className="text-lg">{APP_NAME} leaderboard</span>
+          <span className="text-lg">{APP_NAME} AI Health Advisor</span>
         </a>
       </h2>
 
       <div className="bg-white shadow-lg rounded-lg p-6 max-w-md w-full">
-        <h1 className="text-2xl font-bold mb-4 text-center text-black">
-          AI Health Advisor
-        </h1>
         <div className="mb-4">
           <label className="block text-black">Weight (kg):</label>
           <input
