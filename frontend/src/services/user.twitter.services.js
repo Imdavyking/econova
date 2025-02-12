3c424c4ff35fbf8ac9abbeeefcdf45fe87051b61
imdavyking <yungdynamic53@gmail.com>
2025-02-13 00:31:05 +0100

    chore: updated fronted

--- frontend/src/services/user.twitter.services.js
+++ frontend/src/services/user.twitter.services.js
@@ -3,6 +3,10 @@ import { getTwitterAuth, removeTwitterAuth } from "./twitter.auth.services";
 
 export const getUserTwitterInfo = async () => {
   try {
+    const token = getTwitterAuth();
+    if (!token) {
+      throw new Error("No token found");
+    }
     const response = await fetch(`${SERVER_URL}/api/user`, {
       method: "GET",
       headers: {
