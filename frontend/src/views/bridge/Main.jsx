96b7a9c7c894a6053dd2b9ccb6e94010ecde452a
imdavyking <yungdynamic53@gmail.com>
2025-02-17 11:22:13 +0100

    chore: updated tokenbalance for loading

--- frontend/src/views/bridge/Main.jsx
+++ frontend/src/views/bridge/Main.jsx
@@ -60,7 +60,7 @@ export default function Bridge() {
         setUserBalance(Number(balance) / 10 ** Number(decimals));
       }
     );
-  }, [selectedToken]);
+  }, [selectedToken, loading]);
 
   useEffect(() => {
     if (!sourceChain || !destinationChain || !selectedToken.tokenAddress)
