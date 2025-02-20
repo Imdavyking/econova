a6794dc39e78f606c9298458532a6727cdac57eb
imdavyking <yungdynamic53@gmail.com>
2025-02-20 14:13:38 +0100

    chore: updated allora

--- backend/src/controllers/allora.controllers.ts
+++ backend/src/controllers/allora.controllers.ts
@@ -2,14 +2,18 @@ import { fetchInferenceByTopicID } from "../services/allora.services";
 import { Request, Response } from "express";
 
 export const getPriceInference = async (req: Request, res: Response) => {
-  const { topicId, topicName } = req.query;
-  if (!topicId || !topicName) {
-    res.status(400).json({ error: "Missing topic ID or topic name" });
-    return;
+  try {
+    const { topicId, topicName } = req.query;
+    if (!topicId || !topicName) {
+      res.status(400).json({ error: "Missing topic ID or topic name" });
+      return;
+    }
+    const inference = await fetchInferenceByTopicID(
+      topicName as string,
+      +topicId
+    );
+    res.json({ data: inference });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
   }
-  const inference = await fetchInferenceByTopicID(
-    topicName as string,
-    +topicId
-  );
-  res.json({ data: inference });
 };
