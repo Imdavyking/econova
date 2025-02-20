e2de1fb75eb928fc0c8d6a0242b4f801ac84eac8
imdavyking <yungdynamic53@gmail.com>
2025-02-20 23:38:11 +0100

    chore: added unstoppable domain

---
+++ frontend/src/services/unstoppable.domain.services.js
@@ -0,0 +1,11 @@
+import { Resolution } from "@unstoppabledomains/resolution";
+
+const resolution = new Resolution();
+export const resolveDomainService = async ({ domain, ticker }) => {
+  try {
+    const address = await resolution.addr(domain, ticker);
+    return address;
+  } catch (error) {
+    console.error(error);
+  }
+};
