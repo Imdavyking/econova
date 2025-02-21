46cfa16835c5925fd245f3ec26622178fcfb3bcc
imdavyking <yungdynamic53@gmail.com>
2025-02-21 14:08:17 +0100

    chore: updated ud domains

--- frontend/src/services/unstoppable.domain.services.js
+++ frontend/src/services/unstoppable.domain.services.js
@@ -2,34 +2,13 @@ import { Resolution } from "@unstoppabledomains/resolution";
 
 const resolution = new Resolution();
 
-export const resolveDomainService = async ({ domain, tickers }) => {
+export const resolveDomainService = async ({ domain, ticker }) => {
   try {
-    if (!Array.isArray(tickers) || tickers.length === 0) {
-      throw new Error("Tickers must be a non-empty array");
-    }
+    const address = await resolution.addr(domain, ticker);
 
-    const results = await Promise.allSettled(
-      tickers.map(async (ticker) => {
-        try {
-          const address = await resolution.addr(domain, ticker);
-          return { success: true, ticker, address };
-        } catch {
-          return { success: false, ticker };
-        }
-      })
-    );
-
-    const successfulResolutions = results
-      .filter((res) => res.status === "fulfilled" && res.value.success)
-      .map((res) => res.value);
-
-    if (successfulResolutions.length > 0) {
-      return successfulResolutions[0]; // Return the first successful resolution
-    } else {
-      throw new Error("All ticker resolutions failed");
-    }
+    return address;
   } catch (error) {
     console.error("Error resolving domain:", error);
-    return { success: false, error: error.message };
+    return null;
   }
 };
