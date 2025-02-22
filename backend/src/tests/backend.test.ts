efa4aefc74a012464401c51d3468758b5b73a45c
imdavyking <yungdynamic53@gmail.com>
2025-02-22 01:50:26 +0100

    chore: fixed test for index

--- backend/src/tests/backend.test.ts
+++ backend/src/tests/backend.test.ts
@@ -6,18 +6,16 @@ const SECONDS = 1000;
 jest.setTimeout(70 * SECONDS);
 
 describe("Backend tests", () => {
-  test("GET /", async () => {
-    const res = await request(app).get("/");
-    expect(res.status).toBe(200);
-    expect(res.headers["content-type"]).toMatch(/text\/html|json/);
-    console.log(res.body);
-    expect(res.body).toBe("Hello World!");
-  });
-
   test("GET /api/tweets", async () => {
     const res = await request(app).get("/api/tweets");
     expect(res.status).toBe(200);
     expect(res.headers["content-type"]).toMatch(/json/);
     expect(Array.isArray(res.body)).toBe(true);
+  });
+  test("GET /", async () => {
+    const res = await request(app).get("/");
+    expect(res.status).toBe(200);
+    expect(res.headers["content-type"]).toMatch(/text\/html|json/);
+    expect(res.text).toBe("Hello World!");
   });
 });
