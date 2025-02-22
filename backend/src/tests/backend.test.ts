d73aeb9d77b5317c671445e7ab0f26143f5fd134
imdavyking <yungdynamic53@gmail.com>
2025-02-22 01:02:04 +0100

    chore: updated

--- backend/src/tests/backend.test.ts
+++ backend/src/tests/backend.test.ts
@@ -2,14 +2,18 @@ import request from "supertest";
 import { app } from "../app";
 
 describe("GET /", () => {
-  it("should return hello world", async () => {
+  it("should return hello world", async (done) => {
     request(app)
       .get("/")
       .expect("Content-Type", /json/)
       .expect(200)
+
       .expect((res) => {
-        if (res.body !== "Hello World!")
-          throw new Error("Hello World! not returned");
+        if (res.text !== "Hello World!") {
+          done("Hello World! not returned");
+          return;
+        }
+        done();
       })
       .end(function (err, res) {
         if (err) throw err;
@@ -18,13 +22,13 @@ describe("GET /", () => {
 });
 
 describe("GET /api/tweets", () => {
-  it("should return a list of tweets", async () => {
+  it("should return a list of tweets", async (done) => {
     request(app)
       .get("/api/tweets")
       .expect("Content-Type", /json/)
       .expect(200)
       .end(function (err, res) {
-        if (err) throw err;
+        if (err) done(err);
       });
   });
 });
