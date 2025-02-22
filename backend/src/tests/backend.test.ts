07724082a20d898ab7199df618ebfaa4a6fa72e5
imdavyking <yungdynamic53@gmail.com>
2025-02-22 02:08:12 +0100

    chore: test working

--- backend/src/tests/backend.test.ts
+++ backend/src/tests/backend.test.ts
@@ -1,9 +1,27 @@
 import request from "supertest";
 import app from "../app";
-import { describe, beforeEach, it, expect, jest } from "@jest/globals";
+import {
+  describe,
+  beforeEach,
+  it,
+  expect,
+  jest,
+  afterEach,
+} from "@jest/globals";
+import type { Server } from "http";
 
 const SECONDS = 1000;
 jest.setTimeout(70 * SECONDS);
+
+// let server: Server;
+
+// beforeEach(() => {
+//   server = app.listen(4000);
+// });
+
+// afterEach(() => {
+//   server.close();
+// });
 
 describe("Backend tests", () => {
   test("GET /api/tweets", async () => {
