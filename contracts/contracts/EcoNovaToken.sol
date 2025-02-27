e7927271f4a087e4b46f39e7e7b5dd7cb1ae4a60
imdavyking <yungdynamic53@gmail.com>
2025-02-28 00:42:46 +0100

    chore: updated

--- contracts/contracts/EcoNovaToken.sol
+++ contracts/contracts/EcoNovaToken.sol
@@ -5,8 +5,10 @@ import {OFT} from "@layerzerolabs/oft-evm/contracts/OFT.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract EcoNovaToken is OFT, ERC20Votes {
+contract EcoNovaToken is OFT, ERC20Votes, ERC20Permit {
     address public immutable DEPLOYER;
     uint256 public immutable MAX_SUPPLY = 21_000_000 * 10 ** decimals();
 
@@ -26,7 +28,7 @@ contract EcoNovaToken is OFT, ERC20Votes {
     )
         OFT("EcoNovaToken", "ENT", lzEndpoint, delegate)
         Ownable(delegate)
-        EIP712("EcoNovaToken", "1")
+        ERC20Permit("EcoNovaToken")
     {
         DEPLOYER = msg.sender;
     }
@@ -50,5 +52,9 @@ contract EcoNovaToken is OFT, ERC20Votes {
         uint256 value
     ) internal override(ERC20, ERC20Votes) {
         super._update(from, to, value);
+    }
+
+    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
     }
 }
