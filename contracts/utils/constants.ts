f3777fa5f89eb9237080fd1dc82fe3b0279962a4
imdavyking <yungdynamic53@gmail.com>
2025-03-02 06:56:25 +0100

    chore: updated

--- contracts/utils/constants.ts
+++ contracts/utils/constants.ts
@@ -1,9 +1,8 @@
 export const QUORUM_PERCENTAGE = 4 // Need 4% of voters to pass
-export const MIN_DELAY = 3600 // 1 hour - after a vote passes, you have 1 hour before you can enact
-// export const VOTING_PERIOD = 45818 // 1 week - how long the vote lasts. This is pretty long even for local tests
+export const MIN_DELAY = 3600 // 1 hour - after a vote passes, you have 1 hour before you can execute it
 export const VOTING_PERIOD = 5 // blocks
 export const VOTING_DELAY = 1 // 1 Block - How many blocks till a proposal vote becomes active
-export const PROPOSAL_THRESHOLD = 1 // 1% - the minimum % of total votes that a proposal must have to be enacted
+export const PROPOSAL_THRESHOLD = 1 // the user voting power needed to create a proposal (1 token)
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
 export const PROPOSAL_DESCRIPTION = "Proposal #1 Add this organization!"
 export const ETH_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
