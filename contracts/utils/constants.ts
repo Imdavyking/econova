79194f59a4ee375fcede05b005409c9eb71be7ce
imdavyking <yungdynamic53@gmail.com>
2025-03-02 07:06:07 +0100

    chore: updated

--- contracts/utils/constants.ts
+++ contracts/utils/constants.ts
@@ -1,9 +1,9 @@
 export const QUORUM_PERCENTAGE = 4 // Need 4% of voters to pass
 export const MIN_DELAY = 3600 // 1 hour - after a vote passes, you have 1 hour before you can execute it
-// sonic avg 3blocks per second
-// 60 * 3 // 180 blocks per minute
-// 180 * 60 // 10800 blocks per hour
-export const VOTING_PERIOD = 10800 // 1 hour - How long a proposal vote is open for
+const BLOCKS_PER_SECOND = 3
+const BLOCKS_PER_MINUTE = 60 * BLOCKS_PER_SECOND
+const BLOCKS_PER_HOUR = 60 * BLOCKS_PER_MINUTE
+export const VOTING_PERIOD = BLOCKS_PER_HOUR // 1 hour - How long a proposal vote is open for
 export const VOTING_DELAY = 1 // 1 Block - How many blocks till a proposal vote becomes active
 export const PROPOSAL_THRESHOLD = 1 // the user voting power needed to create a proposal (1 token)
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
