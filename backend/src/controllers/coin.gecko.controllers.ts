d6db0b67a2c90401c5219a20e22de9229ded69b6
imdavyking <yungdynamic53@gmail.com>
2025-03-06 15:23:57 +0100

    chore: updated

--- backend/src/controllers/coin.gecko.controllers.ts
+++ backend/src/controllers/coin.gecko.controllers.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { fetchPriceFromCoinGecko } from "../services/coingecko.services";
+import initializeRedis from "../utils/redis.app";
+import { REDIS_CACHE_TIME } from "../config/database";
 
 export const getPriceFromCoinGecko = async (req: Request, res: Response) => {
   try {
@@ -18,6 +20,20 @@ export const getPriceFromCoinGecko = async (req: Request, res: Response) => {
       path: path as string,
       queryParams: queryParams as Record<string, any>,
     });
+
+    const cacheKey = `${path}-${JSON.stringify(queryParams)}`;
+    const redis = await initializeRedis();
+
+    if (data) {
+      await redis.set(cacheKey, JSON.stringify(data), "EX", REDIS_CACHE_TIME);
+    }
+
+    if (!data) {
+      const cachedData = await redis.get(cacheKey);
+      if (cachedData) {
+        return res.json({ data: JSON.parse(cachedData) });
+      }
+    }
     res.json({ data });
   } catch (error: any) {
     res.status(500).json({ error: error.message });
