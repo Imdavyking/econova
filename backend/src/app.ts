d15f97fa1362870da676b2b659453bff517bb9f5
imdavyking <yungdynamic53@gmail.com>
2025-03-10 19:26:51 +0100

    chore: updated proxy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import { automateCharityFundDistribution } from "./services/automate.services";
 import app from "./utils/create.server";
 import { environment } from "./utils/config";
 import initializeRedis from "./utils/redis.app";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 dotenv.config();
 
@@ -19,4 +20,12 @@ automateCharityFundDistribution();
 
 // Start the server
 const PORT = environment.PORT || 3000;
+app.use(
+  "/pinata",
+  createProxyMiddleware({
+    target: "https://emerald-odd-bee-965.mypinata.cloud",
+    changeOrigin: true,
+    pathRewrite: { "^/pinata": "/files" },
+  })
+);
 app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
