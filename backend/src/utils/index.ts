73cf02c9ad3296af1660fe4d0b5e0a31f10a5a01
imdavyking <yungdynamic53@gmail.com>
2025-03-20 10:53:56 +0100

    chore: updated time

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -54,8 +54,8 @@ const formatTime = (seconds: number): string => {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
   const secs = seconds % 60;
-  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+  return `${String(hours).padStart(2, "0")}h:${String(minutes).padStart(
     2,
     "0"
-  )}:${String(secs).padStart(2, "0")}`;
+  )}m:${String(secs).padStart(2, "0")}s`;
 };
