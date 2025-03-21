018f7c11c506afcd7b1d7c276978cb24c7d0255e
imdavyking <yungdynamic53@gmail.com>
2025-03-21 04:10:00 +0100

    chore: updated darkmode

--- frontend/src/components/dark-mode-switcher/Main.jsx
+++ frontend/src/components/dark-mode-switcher/Main.jsx
@@ -2,6 +2,7 @@ import { useRecoilValue, useSetRecoilState } from "recoil";
 import { darkModeValue, darkMode as darkModeStore } from "@/stores/dark-mode";
 import dom from "@left4code/tw-starter/dist/js/dom";
 import classnames from "classnames";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 function Main(props) {
   const darkMode = useRecoilValue(darkModeStore);
@@ -19,22 +20,27 @@ function Main(props) {
 
   setDarkModeClass();
   return (
-    <>
-      {/* BEGIN: Dark Mode Switcher */}
-      <div
-        className="dark-mode-switcher cursor-pointer shadow-md fixed bottom-0 right-4 box border rounded-full w-40 h-12 flex items-center justify-center z-50 mb-10 mr-10"
-        onClick={switchMode}
-      >
-        <div className="mr-4 text-slate-600 dark:text-slate-200">Dark Mode</div>
-        <div
-          className={classnames({
-            "dark-mode-switcher__toggle border": true,
-            "dark-mode-switcher__toggle--active": darkMode,
-          })}
-        ></div>
+    <div
+      className="border-slate-700 dark:border-slate-700 dark-mode-switcher cursor-pointer shadow-md fixed bottom-0 right-4 box border rounded-full w-auto md:w-40 h-12 flex items-center justify-center z-50 mb-10 mr-10"
+      onClick={switchMode}
+    >
+      <div className="mr-4 text-slate-600 dark:text-slate-200 hidden md:block">
+        Dark Mode
       </div>
-      {/* END: Dark Mode Switcher */}
-    </>
+      <div
+        className={classnames({
+          "dark-mode-switcher__toggle border hidden md:block": true,
+          "dark-mode-switcher__toggle--active": darkMode,
+        })}
+      ></div>
+      <div className="md:hidden py-2 px-4 h-12 flex items-center">
+        {darkMode ? (
+          <FaMoon className="w-6 h-6" />
+        ) : (
+          <FaSun className="w-6 h-6" />
+        )}
+      </div>
+    </div>
   );
 }
 
